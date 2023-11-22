324604b9046d011cd7cee7b50933e12ebaf01798
Rajpreet Kaur <kaurrajpreet2018@gmail.com>
2023-11-22 20:00:31 +0530

    added group chat backend

---
+++ mentalHealth/src/main/java/com/MentalHealthProject/mentalHealth/entities/ChatMessage.java
@@ -0,0 +1,22 @@
+package com.MentalHealthProject.mentalHealth.entities;
+
+import lombok.*;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private String content;
+    private String sender;
+    private MessageType type;
+
+    public Object getSender() {
+        return sender;
+    }
+
+
+}
---
+++ mentalHealth/src/main/java/com/MentalHealthProject/mentalHealth/entities/MessageType.java
@@ -0,0 +1,8 @@
+package com.MentalHealthProject.mentalHealth.entities;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+}
